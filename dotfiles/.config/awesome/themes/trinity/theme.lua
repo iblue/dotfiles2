503b0b3c0aa2467e3583ef97433c4f590044c60b
Markus Fenske <iblue@gmx.net>
2014-04-06 16:04:41 +0200

    Initial

---
+++ dotfiles/.config/awesome/themes/trinity/theme.lua
@@ -0,0 +1,30 @@
+-- trinity, awesome3 theme
+
+local awful = require("awful")
+awful.util = require("awful.util")
+
+theme = {}
+
+theme.font          = "terminus 9"
+
+theme.wallpaper_cmd = "xsetroot -solid '#000000'"
+
+theme.bg_normal     = "#000000"
+theme.bg_focus      = "#223322"
+theme.bg_urgent     = "#ff0000"
+
+theme.fg_normal     = "#55af66"
+theme.fg_focus      = "#55af66"
+theme.fg_urgent     = "#55af66"
+
+theme.border_width  = 1
+theme.border_normal = "#55af66"
+theme.border_focus  = "#55af66"
+theme.border_marked = "#55af66"
+
+theme.taglist_squares = true
+
+theme.titlebar_close_button = true
+
+return theme
+
