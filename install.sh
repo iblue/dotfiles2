05897b43913a0ad528a729f3bbc1c1b198097823
Markus Fenske <iblue@gmx.net>
2015-01-26 16:32:53 +0100

    Fix install script

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-if ! $(lsb_release -a 2>/dev/null | grep "Ubuntu 13.10" > /dev/null); then
-  echo "This is designed to run on Lubuntu 13.10. This is not even an Ubuntu 13.10. It will break."
-  exit 1
-fi
+#if ! $(lsb_release -a 2>/dev/null | grep "Ubuntu 13.10" > /dev/null); then
+#  echo "This is designed to run on Lubuntu 13.10. This is not even an Ubuntu 13.10. It will break."
+#  exit 1
+#fi
 
-if ! [ -d /etc/xdg/lubuntu ]; then
-  echo "This is designed to run on Lubuntu 13.10. This seems to be an Ubuntu 13.10 or derivative. Use with caution."
-  exit 1
-fi
+#if ! [ -d /etc/xdg/lubuntu ]; then
+#  echo "This is designed to run on Lubuntu 13.10. This seems to be an Ubuntu 13.10 or derivative. Use with caution."
+#  exit 1
+#fi
 
 # Install dotfiles
-cp -rf dotfiles/.[a-z]* ~
+cp -rf dotfiles/.[a-zA-Z]* ~
 
 # Window manager: Awesome 3.5 (lubuntu bundles 3.4)
 sudo add-apt-repository ppa:klaus-vormweg/awesome
